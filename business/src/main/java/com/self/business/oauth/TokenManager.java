4b5526c6330a01378a9452fb8d72c0281ff88f1d
harith <harith@yahoo-inc.com>
2014-08-02 17:09:50 -0700

    new class and pom changes

---
+++ business/src/main/java/com/self/business/oauth/TokenManager.java
@@ -0,0 +1,13 @@
+package com.self.business.oauth;
+
+import java.util.UUID;
+
+import javax.annotation.Nonnull;
+import javax.ejb.Stateless;
+
+@Stateless
+public class TokenManager {
+    public String getAccessToken(@Nonnull final String clientId, @Nonnull final String clientSecret) {
+        return UUID.randomUUID().toString();
+    }
+}
