1d12c10e80ece1991ea30293b345fe1dffadde64
harith <harith@yahoo-inc.com>
2014-08-05 18:35:47 -0700

    don't make UserService as bean

--- persistence/src/main/java/com/self/oauth/persistence/entities/UserService.java
+++ persistence/src/main/java/com/self/oauth/persistence/entities/UserService.java
@@ -1,11 +1,9 @@
 package com.self.oauth.persistence.entities;
 
 import javax.annotation.Nonnull;
-import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
-@Stateless
 public class UserService {
     private final EntityManager entityManager;
 
