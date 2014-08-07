31ab7dfc446d582d7b0f0d4125ff5f08495eb3f0
harith <harith@yahoo-inc.com>
2014-08-06 22:28:20 -0700

    new formattting, added notion of LocalDateTime.now().getNano for security reasons

--- business/src/main/java/com/self/oauth/business/oauth/CryptoGenerator.java
+++ business/src/main/java/com/self/oauth/business/oauth/CryptoGenerator.java
@@ -3,42 +3,49 @@ package com.self.oauth.business.oauth;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
 
 import javax.annotation.Nonnull;
 
 public class CryptoGenerator {
-    private String serverPrivateKey;
+	private String serverPrivateKey;
 
-    @SuppressWarnings("UnusedDeclaration")
-    public CryptoGenerator() {
-    }
+	@SuppressWarnings("UnusedDeclaration")
+	public CryptoGenerator() {
+	}
 
-    public CryptoGenerator(@Nonnull final String serverPrivateKey) {
-        this.serverPrivateKey = serverPrivateKey;
-    }
+	public CryptoGenerator(@Nonnull final String serverPrivateKey) {
+		this.serverPrivateKey = serverPrivateKey;
+	}
 
-    @Nonnull
-    public String getClientId(@Nonnull final String email, @Nonnull final String userExternalId) {
-        final String stringToHash = getStringToHash(email, userExternalId);
-        return getHashedClientId(stringToHash);
-    }
+	@Nonnull
+	public String getClientId(@Nonnull final String email, @Nonnull final String userExternalId) {
+		final String stringToHash = getStringToHash(email, userExternalId);
+		return getHashedClientId(stringToHash);
+	}
 
-    @Nonnull
-    private static String getHashedClientId(@Nonnull final String stringToHash) {
-        final MessageDigest messageDigest;
+	@Nonnull
+	private static String getHashedClientId(@Nonnull final String stringToHash) {
+		final MessageDigest messageDigest;
 
-        try {
-            messageDigest = MessageDigest.getInstance("MD5");
-        } catch (final NoSuchAlgorithmException e) {
-            throw new RuntimeException("can not generate client id: " + e.getMessage());
-        }
+		try {
+			messageDigest = MessageDigest.getInstance("MD5");
+		} catch (final NoSuchAlgorithmException e) {
+			throw new RuntimeException("can not generate client id: " + e.getMessage());
+		}
 
-        messageDigest.update(stringToHash.getBytes(), 0, stringToHash.length());
-        return new BigInteger(1, messageDigest.digest()).toString();
-    }
+		messageDigest.update(stringToHash.getBytes(), 0, stringToHash.length());
+		return new BigInteger(1, messageDigest.digest()).toString();
+	}
 
-    @Nonnull
-    private String getStringToHash(@Nonnull final String email, @Nonnull final String userExternalId) {
-        return ":" + email + ":" + serverPrivateKey + userExternalId + ":";
-    }
+	@Nonnull
+	private String getStringToHash(@Nonnull final String email, @Nonnull final String userExternalId) {
+		/**
+		 * generating clientId for same values will be different for security reasons.
+		 * This is why once clientId created will be stored in database.
+		 * The notion of adding LocalDateTime.now().getNano() is to make sure if we ever need to generate/refresh
+		 * a new clientId, it is possible with the same data passed in.
+		 */
+		return ":" + email + ":" + serverPrivateKey + userExternalId + ":" + LocalDateTime.now().getNano() + ":";
+	}
 }
--- business/src/test/java/com/self/oauth/business/oauth/CryptoGeneratorTest.java
+++ business/src/test/java/com/self/oauth/business/oauth/CryptoGeneratorTest.java
@@ -1,7 +1,6 @@
 package com.self.oauth.business.oauth;
 
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 import java.util.UUID;
@@ -9,13 +8,12 @@ import java.util.UUID;
 import org.junit.Test;
 
 public class CryptoGeneratorTest {
-    @Test
-    public void testGetClientId() {
-        final CryptoGenerator cryptoGenerator = new CryptoGenerator("testClientId");
-        final String email = "uniqueEmail@gmail.com";
-        final String userExternalId = UUID.randomUUID().toString();
+	@Test
+	public void testGetClientIdTwiceReturnsDifferntClientIds() {
+		final CryptoGenerator cryptoGenerator = new CryptoGenerator("testClientId");
+		final String email = "uniqueEmail@gmail.com";
+		final String userExternalId = UUID.randomUUID().toString();
 
-        assertEquals(cryptoGenerator.getClientId(email, userExternalId), cryptoGenerator.getClientId(email, userExternalId));
-        assertNotEquals(cryptoGenerator.getClientId(email, userExternalId), cryptoGenerator.getClientId(email + "1", userExternalId));
-    }
+		assertNotEquals(cryptoGenerator.getClientId(email, userExternalId), cryptoGenerator.getClientId(email, userExternalId));
+	}
 }
