6efff034e315dc470dd5bd89ca4dc4dd0d2a280f
harith <harith@yahoo-inc.com>
2014-08-11 08:53:32 -0700

    print headers

--- services/src/main/java/com/self/oauth/services/filter/AuthTokenValidatorFilter.java
+++ services/src/main/java/com/self/oauth/services/filter/AuthTokenValidatorFilter.java
@@ -1,6 +1,7 @@
 package com.self.oauth.services.filter;
 
 import java.io.IOException;
+import java.util.Enumeration;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -9,6 +10,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 
 @WebFilter("/rest/*")
 public class AuthTokenValidatorFilter implements Filter {
@@ -19,7 +21,14 @@ public class AuthTokenValidatorFilter implements Filter {
 
 	@Override
 	public void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse, final FilterChain filterChain) throws IOException, ServletException {
-		System.out.println("filtering request");
+		final HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+		final Enumeration<String> headerNames = httpRequest.getHeaderNames();
+		if (headerNames != null) {
+			while (headerNames.hasMoreElements()) {
+				final String element = headerNames.nextElement();
+				System.out.println("{header} " + element + ":" + httpRequest.getHeader(element));
+			}
+		}
 		filterChain.doFilter(servletRequest, servletResponse);
 	}
 
